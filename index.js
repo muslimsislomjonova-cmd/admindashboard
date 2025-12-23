4137380e57b737c5b64662a90e5a981bffe4af42
muslimsislomjonova-cmd <muslimsislomjonova@gmail.com>
2025-12-23 22:14:25 +0500

    add

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const token = localStorage.getItem("token");
 
 if (!token) {
-  window.location.href = "./index.html"; 
+  window.location.href = "./index.html";
 }
 
+
+let products = [];
+let editIndex = null;
+
+
 fetch("https://fakestoreapi.com/products")
-  .then(res => res.json())
-  .then(data => {
-    renderTable(data);
+  .then(function (res) {
+    return res.json();
   })
-  .catch(err => {
-    console.log("API xato:", err);
+  .then(function (data) {
+    products = data;
+    renderTable();
+  })
+  .catch(function (error) {
+    console.log("Xatolik:", error);
   });
 
-function renderTable(products) {
+
+function renderTable() {
   const tbody = document.getElementById("tableBody");
-
-  if (!tbody) {
-    console.log("tableBody topilmadi");
-    return;
-  }
-
   tbody.innerHTML = "";
 
   products.forEach(function (item, index) {
@@ -29,62 +32,72 @@ function renderTable(products) {
         <td>${index + 1}</td>
         <td>${item.title}</td>
         <td>${item.category}</td>
-        <td>${item.description.slice(0, 80)}...</td>
+        <td>${item.description.slice(0, 40)}...</td>
         <td>${item.price}$</td>
         <td>
-          <img src="${item.image}" width="50">
+          <img src="${item.image}" width="40">
         </td>
         <td>
-          <button>View</button>
-          <button>Edit</button>
-          <button>Delete</button>
+          <button onclick="editProduct(${index})">Edit</button>
+          <button onclick="deleteProduct(${index})">Delete</button>
         </td>
       </tr>
     `;
   });
+}
+
+
+function openAddModal() {
+  editIndex = null;
+  document.getElementById("modalTitle").innerText = "Add Product";
+  clearInputs();
+  document.getElementById("modal").style.display = "flex";
+}
+
+
+
+
+function saveProduct() {
+  const product = {
+    title: titleInput.value,
+    category: categoryInput.value,
+    price: priceInput.value,
+    description: descInput.value,
+    image: imageInput.value
+  };
+
+  if (editIndex === null) {
+    products.push(product);
+  } else {
+    products[editIndex] = product;
+  }
+
+  renderTable();
+  closeModal();
+}
+
+
+function deleteProduct(index) {
+  products.splice(index, 1);
+  renderTable();
+}
+
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+
+function clearInputs() {
+  titleInput.value = "";
+  categoryInput.value = "";
+  priceInput.value = "";
+  descInput.value = "";
+  imageInput.value = "";
 }
 
 
 function logout() {
   localStorage.removeItem("token");
-  window.location.href = "../login.html"; 
+  window.location.href = "../login.html";
 }
-
-
-
-
-
-function renderTable(products) {
-  const tbody = document.getElementById("tableBody");
-
-  if (!tbody) {
-    console.log("tableBody topilmadi");
-    return;
-  }
-
-  tbody.innerHTML = "";
-
-  products.forEach(function (item, index) {
-    tbody.innerHTML += `
-      <tr>
-        <td>${index + 1}</td>
-        <td>${item.title}</td>
-        <td>${item.category}</td>
-        <td>${item.description.slice(0, 80)}...</td>
-        <td>${item.price}$</td>
-        <td>
-          <img src="${item.image}" width="50">
-        </td>
-        <td>
-          <button>View</button>
-          <button>Edit</button>
-          <button onclick="deleteProduct(${item.id})">Delete</button>
-        </td>
-      </tr>
-    `;
-  });
-}
-
-
-
-
