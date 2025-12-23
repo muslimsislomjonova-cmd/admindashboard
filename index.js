e5bf51436a21f65a1efbc6ce4c941c5cce32ff1b
muslimsislomjonova-cmd <muslimsislomjonova@gmail.com>
2025-12-23 13:27:24 +0500

    delete

--- index.js
+++ index.js
@@ -49,3 +49,42 @@ function logout() {
   localStorage.removeItem("token");
   window.location.href = "../login.html"; 
 }
+
+
+
+
+
+function renderTable(products) {
+  const tbody = document.getElementById("tableBody");
+
+  if (!tbody) {
+    console.log("tableBody topilmadi");
+    return;
+  }
+
+  tbody.innerHTML = "";
+
+  products.forEach(function (item, index) {
+    tbody.innerHTML += `
+      <tr>
+        <td>${index + 1}</td>
+        <td>${item.title}</td>
+        <td>${item.category}</td>
+        <td>${item.description.slice(0, 80)}...</td>
+        <td>${item.price}$</td>
+        <td>
+          <img src="${item.image}" width="50">
+        </td>
+        <td>
+          <button>View</button>
+          <button>Edit</button>
+          <button onclick="deleteProduct(${item.id})">Delete</button>
+        </td>
+      </tr>
+    `;
+  });
+}
+
+
+
+
